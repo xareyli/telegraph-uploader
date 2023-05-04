22d027765d868058c924de5cd525d227bc7811a6
alexwaiten <alexwaiten4@gmail.com>
2023-05-04 10:38:36 +0300

    feat: reduce image uploading time

--- API/telegraph.py
+++ API/telegraph.py
@@ -3,6 +3,7 @@ import os
 from utils import getImageExtension, html_to_nodes
 import logging
 import json
+import threading
 
 
 class Telegraph:
@@ -30,31 +31,29 @@ class Telegraph:
     def upload(self, access_token, imgDir, callback):
         html_content = ""
 
-        number_uploaded = 0
+        number_uploaded = NumberUploadedDTO()
 
         for path,_,files in os.walk(imgDir):
-            for name in files:
-                fullpath = os.path.join(path,name)
-                number_uploaded += 1
+            l = len(files) // 3
 
-                with open(fullpath, 'rb') as imageF:
-                    image_extension = getImageExtension(fullpath)
+            ftFiles = files[:l] # first thread files
+            stFiles = files[l:l * 2] # second thread files
+            ttFiles = files[l * 2:] # third thread files
 
-                    response = requests.post(
-                        'https://telegra.ph/upload',
-                        files={'file': ('file', imageF, 'image/{}'.format(image_extension))}
-                    )
+            ft = UploadImageThread(ftFiles, path, number_uploaded, callback)
+            ft.start()
 
-                    image_uploaded = json.loads(response.text)
+            st = UploadImageThread(stFiles, path, number_uploaded, callback)
+            st.start()
 
-                    if not ('error' in image_uploaded):
-                        src = image_uploaded[0]['src']
+            tt = UploadImageThread(ttFiles, path, number_uploaded, callback)
+            tt.start()
 
-                        html_content = html_content + "<img src='{}' />".format(src)
-                        callback(True, number_uploaded, fullpath)
-                    else:
-                        logging.warning('[API]: skipped "{}" due to "{}"'.format(fullpath, image_uploaded['error']))
-                        callback(False, number_uploaded, fullpath)
+            ft.join()
+            st.join()
+            tt.join()
+
+            html_content = ft.html_content + st.html_content + tt.html_content
 
         content_to_be_sent = json.dumps(html_to_nodes(html_content))
 
@@ -72,3 +71,42 @@ class Telegraph:
             return result['result']['url']
         else:
             return False
+
+class NumberUploadedDTO:
+    i = 0
+
+class UploadImageThread(threading.Thread):
+    def __init__(self, *args):
+        threading.Thread.__init__(self)
+        self.html_content = ''
+
+        self.args = args
+
+    def run(self):
+        images = self.args[0]
+        path = self.args[1]
+        number_uploaded = self.args[2]
+        callback = self.args[3]
+
+        for name in images:
+            fullpath = os.path.join(path,name)
+            number_uploaded.i += 1
+
+            with open(fullpath, 'rb') as imageF:
+                image_extension = getImageExtension(fullpath)
+
+                response = requests.post(
+                    'https://telegra.ph/upload',
+                    files={'file': ('file', imageF, 'image/{}'.format(image_extension))}
+                )
+
+                image_uploaded = json.loads(response.text)
+
+                if not ('error' in image_uploaded):
+                    src = image_uploaded[0]['src']
+
+                    self.html_content = self.html_content + "<img src='{}' />".format(src)
+                    callback(True, number_uploaded.i, fullpath)
+                else:
+                    logging.warning('[API]: skipped "{}" due to "{}"'.format(fullpath, image_uploaded['error']))
+                    callback(False, number_uploaded.i, fullpath)
